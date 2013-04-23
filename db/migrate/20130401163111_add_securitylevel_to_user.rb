da0d84fe0005b3df4cfff360ad1431424cf7e178
Simon Howes <si_howes@hotmail.com>
2013-04-23 18:48:54 +0100

    removed some files

--- db/migrate/20130401163111_add_securitylevel_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddSecuritylevelToUser < ActiveRecord::Migration
-  def change
-  	add_column :users, :securitylevel, :string
-  end
-end
--- db/migrate/20130401181939_add_access_for_adminuser.rb
+++
@@ -1,8 +0,0 @@
-class AddAccessForAdminuser < ActiveRecord::Migration
-  def up
-  	update('UPDATE User SET Admin = "true" where User_id = "13"')
-  end
-
-  def down
-  end
-end
